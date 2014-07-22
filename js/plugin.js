3933f7c8e42f53a881c1c19c79d4fbd09e83b2bc
jonnykry <jonnykry93@gmail.com>
2014-07-22 00:57:14 -0500

    Finished the widget on my site.  SO AWESOMEgit add -A!

--- js/plugin.js
+++ js/plugin.js
@@ -1,33 +1,75 @@
 // To use this widget, edit your username / api key into the URL
-// please respect my API key if you use it... I trust you :')
-// var xml = httpGet("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jonnydoesmusic&api_key=c64600ddca04dfc310703c59fe1b5230");
+// Please avoid using my key :')
 
-// var track = xml.getElementsByTagName("track");
-// var i = 0;
+var artist = null;
+var song = null;
+var streamable = null;
+var album = null;
+var albumimg = null;
+var txt = "";
+var scrollYpos = null;
 
-// var txt = displayTrack(track);
+var xml = httpGet("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jonnydoesmusic&api_key=c64600ddca04dfc310703c59fe1b5230");
 
-// document.write(txt);
+var track = xml.getElementsByTagName("track");
+var i = 0;
 
-// $("#widget").append(text);
+var txt = displayTrack(track);
 
-// function httpGet(theUrl)
-// {
-//     var xmlHttp = null;
-//     xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open("GET", theUrl, false);
-//     xmlHttp.send(null);
-//     return xmlHttp.responseXML;
-// }
+document.write(txt);
 
-// function displayTrack(track)
-// {
-//     artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
-//     song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-//     streamable = (track[0].getElementsByTagName("streamable")[0].childNodes[0].nodeValue);
-//     album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
-    
-//     txt = "<div id='text' style='padding-top: 5px; color: #FFF;'><marquee behavior='scroll' scrollamount='5' direction='left' width='350'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank' style='color: white; font-family: Arial;'><i class='fa fa-music'></i> &mdash; Currently listening to: " 
-//     + artist + ' - "' + song + '"' + " &mdash; Click to view my LastFM" + " &mdash; <i class='fa fa-music'></i></a></marquee></div>";
-//     return txt;
-// }
+$("#widget").append(text);
+
+// Hides widget on landing page
+$(window).scroll(function() {
+	scrollYpos = $(document).scrollTop();
+	if(scrollYpos == 0) {
+		$('#widget').css({'visibility':'hidden'});
+	}
+	else {
+		$("#widget").css({'visibility':'visible'});
+	}
+});
+
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseXML;
+}
+
+function displayTrack(track)
+{
+    // Sometimes LastFM does not supply the image. This will account for it.
+    // This occurs whenever there is a (feat. ____) tag on the current song.
+    // Taking the song before it works.
+    try {
+        artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
+        song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+
+        // Take prev. song instead
+        if(song.indexOf('(') > -1) {
+        	artist = (track[1].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
+        	song = (track[1].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+        	album = (track[1].getElementsByTagName("album")[0].childNodes[0].nodeValue);
+        	albumimg = (track[1].getElementsByTagName("image")[0].childNodes[0].nodeValue);        	      	
+        }
+        else {
+        	// add in album and img from current song
+        	album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
+        	albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);   
+        }
+    } catch(err) {
+	    txt = "<div id='text'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank'>" 
+	    + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + ' - "' + song + '"' + "</strong></a></div>";
+		return txt;
+    }
+
+    // This text contains the html that is appended to that page.
+    // Edit the link to contain your username. 
+    txt = "<div id='text'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank'>" 
+    + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + ' - "' + song + '"' + "</strong></a></div>";
+    return txt;
+}
